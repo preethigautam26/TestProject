4b24799f1061e3970a0bea599ec1ca89f2a6fa9c
Amith Gautam <Amith Gautam@LAPTOP-UC8VD6T3>
2024-06-25 14:37:03 +0530

    modifying data

--- AmithArtifactID/src/main/java/javaPackage/SampleProgram.java
+++ AmithArtifactID/src/main/java/javaPackage/SampleProgram.java
@@ -5,7 +5,7 @@ public class SampleProgram {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello main method");
-		System.out.println("Hello Git");
+		System.out.println("Modifying data");
 
 	}
 
